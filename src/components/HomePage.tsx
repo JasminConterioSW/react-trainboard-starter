b910660b46da0cfbe7d9dbd1c46b48830a1d786b
JasCon <jasmin.conterio@softwire.com>
2024-09-18 17:30:00 +0100

    Pass option names into Selector

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Selector from './Selector';
 
 const HomePage: React.FC = () => {
+    const stationNames: string[] = ['A', 'B', 'C', 'D', 'E'];
 
     return (
         <div>
             <h1>Lovely home page</h1>
-            <Selector dropdownName = "Station From"/>
-            <Selector dropdownName = "Station To"/>
+            <Selector dropdownName = "Station From" options = { stationNames } />
+            <Selector dropdownName = "Station To" options = { stationNames }/>
             <button>Stations picked</button>
         </div>
     );
--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Dropdown, DropdownButton } from 'react-bootstrap';
 
 interface Props {
     dropdownName: string;
+    options: string[];
 }
 
-const Selector: React.FC<Props> = ({ dropdownName }) => {
+const Selector: React.FC<Props> = ({ dropdownName, options }) => {
     return (
         <div>
             <h2>{dropdownName}</h2>
             <select>
-                <option>Station 1</option>
-                <option>Station 2</option>
-                <option>Station 3</option>
+                {options.map(option => (<option
+                    key = { option }
+                    value = { option } >{option}
+                </option>))}
             </select>
         </div>
 
